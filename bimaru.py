eaa07cf61131bb865787153feea45c949bd1cbcb
andremcorreia <andrecorreialagos@gmail.com>
2023-05-12 00:34:45 +0100

    + Deterministic Solving functionalty

--- bimaru.py
+++ bimaru.py
@@ -6,6 +6,19 @@
 # 00000 Nome1
 # 00000 Nome2
 
+WATER = "w"
+HINTWATER = "W"
+CIRCLE = "C"
+TOP = "T"
+MIDDLE = "M"
+BOTTOM = "B"
+LEFT = "L"
+RIGHT = "R"
+UNKNOWN = " "
+PART = "X"
+
+
+import numpy as np
 import sys
 from search import (
     Problem,
@@ -17,11 +30,208 @@ from search import (
     recursive_best_first_search,
 )
 
+class Board:
+    """Representação interna de um tabuleiro de Bimaru."""
+    WATER = "w"
+    HINTWATER = "W"
+    CIRCLE = "C"
+    TOP = "T"
+    MIDDLE = "M"
+    BOTTOM = "B"
+    LEFT = "L"
+    RIGHT = "R"
+    UNKNOWN = " "
+    PART = "X"
 
+    def __init__(self):
+        self.board = np.full((10, 10), self.UNKNOWN)
+        self.row_values = []
+        self.col_values = []
+        self.ships = [4, 3, 2, 1]
+
+    def get_value(self, row: int, col: int) -> str:
+        return self.board[row][col]        
+    
+    def set_value(self, row: int, col: int, value) -> str:
+        self.board[row][col] = value
+        
+    def adjacent_vertical_values(self, row: int, col: int):# -> (str, str):
+        """Devolve os valores imediatamente acima e abaixo,
+        respectivamente."""
+        res = []
+        if col != 0: 
+            res += [self.get_value(row - 1, col)]
+        if col != 9:
+            res += [self.get_value(row + 1, col)]
+        return res
+
+    def adjacent_horizontal_values(self, row: int, col: int):# -> (str, str):
+        """Devolve os valores imediatamente à esquerda e à direita,
+        respectivamente."""
+        res = []
+        if col != 0: 
+            res += [self.get_value(row, col - 1)]
+        if col != 9:
+            res += [self.get_value(row, col + 1)]
+        return res
+
+    def get_diagonal_values(self, row: int, col: int):
+        res = []
+        if col != 0: 
+            if row != 0:
+                res += [self.board[row-1][col-1]]
+            if row != 9:
+                res += [self.board[row-1][col-1]]
+        if col != 9:
+            if row != 0:
+                res += [self.board[row-1][col+1]]
+            if row != 9:
+                res += [self.board[row+1][col+1]]
+        return res
+    
+    def fill_water_row(self, row: int):
+        self.board[row] = [self.WATER if value == self.UNKNOWN else value for value in self.board[row]]
+    
+    def fill_ships_row(self, row: int):
+        self.board[row] = [self.PART if value == self.UNKNOWN else value for value in self.board[row]]
+
+    def fill_water_col(self, col: int):
+        for i in range(10):
+            if self.board[i][col] == self.UNKNOWN:
+                self.board[i][col] = self.WATER
+    
+    def fill_ships_col(self, col: int):
+        for i in range(10):
+            if self.board[i][col] == self.UNKNOWN:
+                self.board[i][col] = self.PART
+    
+    def wetDiagonals(self, row: int, col: int):
+        if col != 0: 
+            if row != 0:
+                self.board[row-1][col-1] = WATER
+            if row != 9:
+                self.board[row+1][col-1] = WATER
+        if col != 9:
+            if row != 0:
+                self.board[row-1][col+1] = WATER
+            if row != 9:
+                self.board[row+1][col+1] = WATER
+        return 
+    
+    def wetVerticaly(self, row: int, col: int):
+        if row != 0: 
+            self.board[row-1][col] = WATER
+        if row != 9:
+            self.board[row+1][col] = WATER
+        return
+    
+    def wetSideways(self, row: int, col: int):
+        if col != 0: 
+            self.board[row][col-1] = WATER
+        if col != 9:
+            self.board[row][col+1] = WATER
+        return
+    
+    def assign(self,  row: int, col: int, type: str):
+        if self.board[row][col] != UNKNOWN and self.board[row][col] != type:
+            return False
+        if type != WATER and type != HINTWATER:
+            self.wetDiagonals(row, col)
+        if type == CIRCLE:
+            self.wetVerticaly(row, col)
+            self.wetSideways(row, col)
+        if type == TOP:
+            self.wetSideways(row, col)
+            if row != 0: 
+                self.set_value(row - 1, col, WATER)
+            self.assign(row+1, col, PART)
+        if type == BOTTOM:
+            self.wetSideways(row, col)
+            if row != 9: 
+                self.set_value(row + 1, col, WATER)
+            self.assign(row-1, col, PART)
+        if type == RIGHT:
+            self.wetVerticaly(row, col)
+            if col != 9: 
+                self.set_value(row, col + 1, WATER)
+            self.assign(row, col-1, PART)
+        if type == LEFT:
+            self.wetVerticaly(row, col)
+            if col != 0: 
+                self.set_value(row, col - 1, WATER)
+            self.assign(row, col+1, PART)
+        self.set_value(row, col, type)
+
+    def lineProcesser(self):
+        modified = False
+        for i in range(0,10):
+            rowShips, colShips, rowEmpty, colEmpty = 0,0,0,0
+            for j in range(0,10):
+                if self.board[i][j] == UNKNOWN:
+                    rowEmpty += 1
+                elif self.board[i][j] != WATER and self.board[i][j] != HINTWATER:
+                    rowShips += 1
+                if self.board[j][i] == UNKNOWN:
+                    colEmpty += 1
+                elif self.board[j][i] != WATER and self.board[j][i] != HINTWATER:
+                    colShips += 1
+
+                if self.board[i][j] == MIDDLE:
+                    adjH = self.adjacent_horizontal_values(i,j)
+                    adjV = self.adjacent_vertical_values(i,j)
+                    if WATER in adjH or HINTWATER in adjH or len(adjH) == 1:
+                        if UNKNOWN in adjV:
+                            print(adjV)
+                            modified = True
+                            self.assign(i-1,j,PART)
+                            self.assign(i+1,j,PART)
+                    
+                    if WATER in adjV or HINTWATER in adjV or len(adjV) == 1:
+                        if UNKNOWN in adjH:
+                            print(adjH)
+                            modified = True
+                            self.assign(i,j-1,PART)
+                            self.assign(i,j+1,PART)
+
+            if rowShips == self.row_values[i] and rowEmpty != 0:
+                modified = True
+                self.fill_water_row(i)
+            elif rowShips + rowEmpty == self.row_values[i] and rowEmpty != 0:
+                modified = True
+                self.fill_ships_row(i)
+            if colShips == self.col_values[i] and colEmpty != 0:
+                modified = True
+                self.fill_water_col(i)
+            elif colShips + colEmpty == self.col_values[i]  and colEmpty != 0:
+                modified = True
+                self.fill_ships_col(i)
+        if modified:
+            self.lineProcesser()
+
+    @staticmethod
+    def parse_instance():
+        """Lê o test do standard input (stdin) que é passado como argumento
+        e retorna uma instância da classe Board.
+        """
+        board = Board()
+        filename = sys.argv[1]
+        with open(filename) as f:
+            lines = f.readlines()
+        # Parse the row and column values
+        board.row_values = list(map(int, lines[0].split()[1:]))
+        board.col_values = list(map(int, lines[1].split()[1:]))
+        # Parse the hints and place them on the board
+        for line in lines[3:]:
+            input = line.split()
+            row, col = int(input[1]), int(input[2])
+            letter = input[3]
+            board.assign(row,col,letter)
+        return board
+    
 class BimaruState:
     state_id = 0
 
-    def __init__(self, board):
+    def __init__(self, board: Board):
         self.board = board
         self.id = BimaruState.state_id
         BimaruState.state_id += 1
@@ -31,48 +241,16 @@ class BimaruState:
 
     # TODO: outros metodos da classe
 
-
-class Board:
-    """Representação interna de um tabuleiro de Bimaru."""
-
-    def get_value(self, row: int, col: int) -> str:
-        """Devolve o valor na respetiva posição do tabuleiro."""
-        # TODO
-        pass
-
-    def adjacent_vertical_values(self, row: int, col: int) -> (str, str):
-        """Devolve os valores imediatamente acima e abaixo,
-        respectivamente."""
-        # TODO
-        pass
-
-    def adjacent_horizontal_values(self, row: int, col: int) -> (str, str):
-        """Devolve os valores imediatamente à esquerda e à direita,
-        respectivamente."""
-        # TODO
-        pass
-
-    @staticmethod
-    def parse_instance():
-        """Lê o test do standard input (stdin) que é passado como argumento
-        e retorna uma instância da classe Board.
-
-        Por exemplo:
-            $ python3 bimaru.py < input_T01
-
-            > from sys import stdin
-            > line = stdin.readline().split()
-        """
-        # TODO
-        pass
-
-    # TODO: outros metodos da classe
-
-
 class Bimaru(Problem):
     def __init__(self, board: Board):
-        """O construtor especifica o estado inicial."""
-        # TODO
+        originalState = BimaruState(board)
+        print("input")
+        print(board.board)
+        board.lineProcesser()
+        print("simplified")
+        print(board.board)
+        print(board.row_values)
+        print(board.col_values)
         pass
 
     def actions(self, state: BimaruState):
@@ -100,7 +278,6 @@ class Bimaru(Problem):
         """Função heuristica utilizada para a procura A*."""
         # TODO
         pass
-
     # TODO: outros metodos da classe
 
 
@@ -110,4 +287,6 @@ if __name__ == "__main__":
     # Usar uma técnica de procura para resolver a instância,
     # Retirar a solução a partir do nó resultante,
     # Imprimir para o standard output no formato indicado.
+    originalBoard = Board.parse_instance()
+    game = Bimaru(originalBoard)
     pass
